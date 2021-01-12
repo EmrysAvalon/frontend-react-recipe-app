de0392eafe58ffc1f4d68dc5d092a1fa6eead2e2
Nova Eeken <novaeeken@Novas-MacBook-Pro.local>
2021-01-12 13:52:01 +0100

    Create assignment base

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
+import { recipes } from './assets/recipes';
+import Card from './components/card/Card';
+import Header from './components/header/Header';
+import Button from './components/button/Button';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page-container">
+     <Header />
+      <Button>Ga naar EdHub</Button>
+      <div className="recipe-container">
+        {recipes.map((recipe) => {
+          return (
+            <Card
+              key={recipe.title}
+              img={recipe.image}
+              title={recipe.title}
+              duration={recipe.minutes}
+              highlight={recipe.highlighted}
+            />
+          )
+        })}
+      </div>
+      <Button type="outline">Bekijk meer recepten</Button>
     </div>
   );
 }
